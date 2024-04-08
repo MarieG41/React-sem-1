77d8c6b2840b5c412c5def5bd58338255806e34f
MarieG41 <mariegond8@gmail.com>
2024-04-08 17:42:50 +0200

    exo7 begining

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Dish( {name, img, price} ) {
     return (
-        <div>
-            <h3>{name}</h3>
-            <img src={img} alt="plats méxicains à la carte" />
-            <p>{price}€</p>
-        </div>
+        <Card>
+            <Container className="justify-content-md-center">
+                <Row>
+                    <img src={img} alt="plats méxicains à la carte" />
+                    <Col>
+                        <h2>{name}</h2>
+                        <p>{price}€</p>
+                    </Col>
+                </Row>
+            </Container>
+        </Card>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import React from "react"
+import Navbar from 'react-bootstrap/Navbar'
+import '../styles/header.css'
 
 export default function Header() {
     return (
-        <header>
-            <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" 
+        <Navbar className="bg-body-tertiary">
+            <img className="logo" src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" 
             alt="un mexicain qui joue de la guitare" />
             <nav>
                 <a href="/">Accueil</a>
+                <a href="/">A propos</a>
             </nav>
-        </header>
+        </Navbar>
     )
 }
