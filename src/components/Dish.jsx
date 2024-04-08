61e6859f694736cf6f52b5c4c074de914d3ef1cf
MarieG41 <mariegond8@gmail.com>
2024-04-08 15:51:35 +0200

    main in app added

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Dish( {name, img, price} ) {
     return (
-        <main>
+        <div>
             <h3>{name}</h3>
             <img src={img} alt="plats méxicains à la carte" />
             <p>{price}€</p>
-        </main>
+        </div>
     )
 }
