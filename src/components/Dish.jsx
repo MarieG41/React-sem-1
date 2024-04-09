09745116c4d502913d5315a007e898368fa5735a
MarieG41 <mariegond8@gmail.com>
2024-04-09 10:47:02 +0200

    exo7 done

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import Card from 'react-bootstrap/Card'
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import '../assets/styles/dish.css'
 
 export default function Dish( {name, img, price} ) {
     return (
-        <Card>
-            <Container className="justify-content-md-center">
-                <Row>
-                    <img src={img} alt="plats méxicains à la carte" />
-                    <Col>
-                        <h2>{name}</h2>
-                        <p>{price}€</p>
-                    </Col>
-                </Row>
-            </Container>
-        </Card>
+        <div className="main--div-dish">
+            <Card>
+                <Card.Img variant="top" className="dish-img" src={img} alt={name}/>
+                    <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                        {price}€
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,28 @@
 import React from "react"
 import Navbar from 'react-bootstrap/Navbar'
-import '../styles/header.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import '../assets/styles/header.css'
 
 export default function Header() {
     return (
-        <Navbar className="bg-body-tertiary">
-            <img className="logo" src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" 
-            alt="un mexicain qui joue de la guitare" />
-            <nav>
-                <a href="/">Accueil</a>
-                <a href="/">A propos</a>
-            </nav>
-        </Navbar>
+        <header>
+            <Navbar className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img
+                        src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" 
+                        className="d-inline-block align-top logo"
+                        alt="un mexicain qui joue de la guitare"
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Collapse className="justify-content-end">
+                        <Nav>
+                            <Nav.Link href="/">Accueil</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
     )
 }
