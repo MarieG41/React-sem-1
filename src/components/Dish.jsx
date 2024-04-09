3f85d18eca1a1d9c83307cccda5b66b96cc6542e
MarieG41 <mariegond8@gmail.com>
2024-04-09 17:23:33 +0200

    exo13 works

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -7,18 +7,16 @@ import { Link } from "react-router-dom";
 export default function Dish( {name, img, price, slug} ) {
     return (
         <div className="main--div-dish">
-            <Card>
+            <Link to={`${slug}`}><Card>
                 <Card.Img variant="top" className="dish-img" src={img} alt={name}/>
                     <Card.Body>
                     <Card.Title>{name}</Card.Title>
                     <Card.Text>
                         {price}€
                     </Card.Text>
-                    <Card.Link>
-                        <Link to={slug}>Détails</Link>
-                    </Card.Link>
+                        
                 </Card.Body>
-            </Card>
+            </Card></Link>
         </div>
     )
 }
