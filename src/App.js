07ebf84a952eb1bc9e7d54ca124f75e86989682d
MarieG41 <mariegond8@gmail.com>
2024-04-09 15:30:23 +0200

    exo12

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home"
 import {Route, Routes} from "react-router-dom"
 import Layout from './components/Layout';
 import DishDetails from './pages/DishDetails';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route element={<Layout />}>
             <Route index element={<Home/>} />
             <Route path='plat/:slug' element={<DishDetails />} />
+            <Route path='*' element={<NotFound />} />
           </Route>
         </Routes>
     </div>
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+export default function NotFound() {
+    return (
+        <div>
+            <Container>
+                <Row><h1>Page non trouvée</h1></Row>
+            </Container>
+        </div>
+    )
+}
