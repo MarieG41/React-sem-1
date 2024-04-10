0684caa365c6185e1a9b13fe6bc695d4825310f1
MarieG41 <mariegond8@gmail.com>
2024-04-10 14:32:46 +0200

    exo16

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import Card from 'react-bootstrap/Card'
 import '../assets/styles/dish.css'
 import { Link } from "react-router-dom";
+import Badge from 'react-bootstrap/Badge';
 
-export default function Dish( {name, img, price, slug} ) {
+export default function Dish({ name, img, price, slug, isNew }) {
     return (
         <div className="main--div-dish">
             <Link to={`plat/${slug}`}><Card>
+                {isNew && <Badge bg="primary">Nouveau</Badge>}
                 <Card.Img variant="top" className="dish-img" src={img} alt={name}/>
                     <Card.Body>
-                    <Card.Title>{name}</Card.Title>
-                    <Card.Text>
-                        {price}€
-                    </Card.Text>
-                        
+                        <Card.Title>{name}</Card.Title>
+                        <Card.Text>
+                            {price}€
+                        </Card.Text>
                 </Card.Body>
             </Card></Link>
         </div>
