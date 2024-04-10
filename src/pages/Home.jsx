5bcbde3cc4593fe924012de46e8c46a344184aa0
MarieG41 <mariegond8@gmail.com>
2024-04-10 12:08:31 +0200

    cleaning import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import Dish from '../components/Dish';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Stack from 'react-bootstrap/Stack'
 
 export default function Home() {
